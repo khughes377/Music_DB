bd767e7a4a5314d52f77069498dce2299a66cdf0
khughes377 <khughes377@gmail.com>
2015-02-25 09:11:23 -0500

    first commit of readme

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,8 @@
 class Genre < ActiveRecord::Base
+	has_many :artists
+	has_many :songs
+	has_many :albums
+
+	validates :name, presence: true
+	validates :name, length: {minimum: 2}
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,7 @@
 class Song < ActiveRecord::Base
+	belongs_to :artist
+	belongs_to :album
+	belongs_to :genre
+
+	validates :name, presence: true
 end
